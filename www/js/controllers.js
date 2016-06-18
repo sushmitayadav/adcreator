e64fe474515a8b33afebdeb94b433db13905eb62
sushmitayadav <sushmitayadav735@gmail.com>
2016-06-18 08:25:53 +0530

    First commit

---
+++ www/js/controllers.js
@@ -0,0 +1,42 @@
+angular.module('app.controllers', [])
+  
+.controller('textCtrl', function($scope) {
+
+})
+   
+.controller('imageCtrl', function($scope) {
+
+})
+   
+.controller('videoCtrl', function($scope) {
+
+})
+         
+.controller('adCreatorStudioCtrl', function($scope) {
+
+})
+   
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('signupCtrl', function($scope) {
+
+})
+   
+.controller('welcomeCtrl', function($scope) {
+
+})
+   
+.controller('image2Ctrl', function($scope) {
+
+})
+   
+.controller('previewCtrl', function($scope) {
+
+})
+   
+.controller('adSizeCtrl', function($scope) {
+
+})
+ 
